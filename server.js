b856f6be0ad1a35274752fa6a9c729ce1de0cfe9
SuryaKarmakar <surjokarmakarssj7@gmail.com>
2021-12-12 13:09:38 +0530

    routing

--- server.js
+++ server.js
@@ -3,8 +3,21 @@ const http = require("http");
 const PORT = 8000;
 
 const requestListener = (req, res) => {
-  res.write("<h1>Welcome to vanilla node server</h1>");
-  res.end();
+  const url = req.url;
+
+  if (url === "/") {
+    res.write("<h1>Welcome to vanilla node server</h1>");
+    res.end();
+  } else if (url === "/home") {
+    res.write("<h1>Home Page</h1>");
+    res.end();
+  } else if (url === "/about") {
+    res.write("<h1>About Page</h1>");
+    res.end();
+  } else {
+    res.write("<h1>Page Not Found</h1>");
+    res.end();
+  }
 };
 const server = http.createServer(requestListener);
 
